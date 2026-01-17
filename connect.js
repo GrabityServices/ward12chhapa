517c21a7c285d746461e45d90ec595801a178152
DevendraKumar-JAJ <dkumar06545@gmail.com>
2026-01-17 10:56:53 +0530

    Text file

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
+console.log (process.env.MONGODB); 
 mongoose
-  .connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true } )
+  .connect(process.env.MONGODB)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Error", err));
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
+require("dotenv").config();
 const path = require("path");
 const Registration = require("./models/form.js");
 require("./connect.js");
-require("dotenv").config();
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+console.log (process.env.PORT); 
 const app = express();
 
 // Set EJS as template engine
